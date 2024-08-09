6d074f0166fcfe1d8cf2a7fbdd6cc4ce645a2a05
Levi <64612246+Carlos-Levi@users.noreply.github.com>
2024-08-09 15:17:03 +0000

    add funcionalidade Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Header.module.css';
 
 const Header: React.FC = () => {
+  const [activeSection, setActiveSection] = useState<string>('');
+
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
+      setActiveSection(sectionId); // Atualiza a seção ativa
+    } else {
+      console.warn(`Section with ID "${sectionId}" not found.`);
     }
   };
 
@@ -17,15 +22,32 @@ const Header: React.FC = () => {
         <div className={styles.logo}>LV</div>
       </div>
       <div className={styles.navContainer}>
-        <div onClick={() => scrollToSection('home')} className={styles.navItem}>
-          <div className={styles.navTextActive}>HOME</div>
+        <div
+          onClick={() => scrollToSection('home')}
+          className={styles.navItem}
+        >
+          <div className={activeSection === 'home' ? styles.navTextActive : styles.navText}>
+            HOME
+          </div>
           <div className={styles.underline}></div>
         </div>
-        <div onClick={() => scrollToSection('work')} className={styles.navItem}>
-          <div className={styles.navText}>Work</div>
+        <div
+          onClick={() => scrollToSection('work')}
+          className={styles.navItem}
+        >
+          <div className={activeSection === 'work' ? styles.navTextActive : styles.navText}>
+            Work
+          </div>
+          <div className={styles.underline}></div>
         </div>
-        <div onClick={() => scrollToSection('about')} className={styles.navItem}>
-          <div className={styles.navText}>About</div>
+        <div
+          onClick={() => scrollToSection('about')}
+          className={styles.navItem}
+        >
+          <div className={activeSection === 'about' ? styles.navTextActive : styles.navText}>
+            About
+          </div>
+          <div className={styles.underline}></div>
         </div>
       </div>
     </div>
